9dcbcbf29b2603c13a45583d186318f8933999f9
RafaelGranza <37052163+RafaelGranza@users.noreply.github.com>
2020-08-24 17:26:27 -0300

    Create sumoffloor.cpp

---
+++ Matematica/sum of floor(x|n)/sumoffloor.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 1e9 + 7 ;
+
+long long sumoffloor(long long n){
+    long long answer = 0;
+    int i;
+    for (i = 1; (long long)i*i <= n; i++) {
+        answer += n/i;
+        answer %= MOD;
+    }
+    i--;
+    for (int j = 1; n/(j+1) >= i; j++) {
+        answer += (((n/j - n/(j+1)) % MOD) * j) % MOD;
+        answer %= MOD;
+    }
+    return  answer;
+}
+
+int main() {
+    long long n;
+    scanf("%lld", &n);
+    printf("%lld\n", sumoffloor(n));
+}
